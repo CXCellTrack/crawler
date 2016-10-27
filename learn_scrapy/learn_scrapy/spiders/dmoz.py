6400bbdfdc0323da6eef65a8b1066d7f20cc4b1a
chenxu <xchen-lol@qq.com>
2016-10-27 17:37:45 +0800

    学习scrapy

---
+++ learn_scrapy/learn_scrapy/spiders/dmoz.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from learn_scrapy.items import LearnScrapyItem
+
+
+class DmozSpider(scrapy.Spider):
+    name = "dmoz"
+    allowed_domains = ["dmoz.org"]
+    start_urls = ['http://www.dmoz.org/Computers/Programming/Languages/Python/Books/']
+
+    def parse(self, response):
+        for sel in response.xpath('//*[@id="main-nav"]/ul/li'):
+            item = LearnScrapyItem()
+            item['link'] = sel.xpath('a/@href').extract()[0].strip()
+            item['desc'] = sel.xpath('a/text()').extract()[0].strip()
+            yield item
+
+
